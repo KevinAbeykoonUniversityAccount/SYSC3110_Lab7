fed0f45fe3c3294a557c062fcdc32b6d9ae51335
Kevin Abeykoon <abeykoonkevin@gmail.com>
2025-11-06 10:25:40 -0500

    removing old functions

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -2,12 +2,6 @@ public class LifelineSite extends Site{
     int _units = 12;
     double _rate = 0.10;
 
-    public double getBillableAmount(int _units, int _rate){
-        double base = _units * _rate * 0.5;
-        double tax = base * Site.TAX_RATE * 0.2;
-        return base + tax;
-    }
-
     public static void main(String[] args) {
 
     }
--- src/ResidentialSite.java
+++ src/ResidentialSite.java
@@ -2,12 +2,6 @@ public class ResidentialSite extends Site{
     int _units = 10;
     double _rate = 0.19;
 
-    public double getBillableAmount(){
-        double base = _units * _rate;
-        double tax = base * Site.TAX_RATE;
-        return base + tax;
-    }
-
     public static void main(String[] args) {
 
     }
