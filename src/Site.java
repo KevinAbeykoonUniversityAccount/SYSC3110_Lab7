a95f1b6934de953e38d696db6bf529d31d0e9652
Kevin Abeykoon <abeykoonkevin@gmail.com>
2025-11-06 10:14:38 -0500

    creating functions in site

--- src/Site.java
+++ src/Site.java
@@ -1,3 +1,15 @@
 public class Site {
     public static double TAX_RATE = 0.13;
+
+    public double getBillableAmount(){
+        return 0.0;
+    }
+
+    public double getBaseAmount(){
+        return 0.0;
+    }
+
+    public double getTaxAmount(){
+        return 0.0;
+    }
 }
